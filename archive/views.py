3ede120471d1b21de9475654d5bb8bd2f9470384
Walery <waleron@gmail.com>
2024-03-30 15:50:49 +0100

    Add urls, change archive admin, models and views

--- archive/views.py
+++ archive/views.py
@@ -1,3 +1,13 @@
+from django.utils import timezone
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'archive/post_list.html', {'posts': posts})
+
+
+def error_404_view(request, exception):
+    data = {}
+    return render(request, 'archive/404.html', data)
